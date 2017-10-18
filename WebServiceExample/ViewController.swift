55d32bb0012d675e5d48208d6717cac4198901aa
Bernard Desert <bdesert@etsy.com>
2017-10-18 14:09:18 -0400

    Add JSON, AFNetworking, TODOs

--- WebServiceExample/ViewController.swift
+++ WebServiceExample/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // TODO: Replace the appid code with the "API Key"  , Update Info.plist Unit4 Lesson2 Calling Web Service  , try challenge listed end of Unit4 Lesson 2 
+    
+    @IBOutlet weak var forecastLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = AFHTTPSessionManager()
+        
+        manager.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=2de143494c0b295cca9337e1e96b00e0",
+                    parameters: nil,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject:Any?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + (responseObject as AnyObject).description)
+                            if let listOfDays = (responseObject as AnyObject)["list"] as? [AnyObject] {
+                                if let tomorrow = listOfDays[0] as? [String:AnyObject] {
+                                    if let tomorrowsWeather = tomorrow["weather"] as? [AnyObject] {
+                                        if let firstWeatherOfDay = tomorrowsWeather[0] as? [String:AnyObject] {
+                                            if let forecast = firstWeatherOfDay["description"] as? String {
+                                                self.forecastLabel.text = forecast
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+        }) { (operation:URLSessionDataTask?, error:Error) in
+            print("Error: " + error.localizedDescription)
+            
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  WebServiceExample
+//
+//  Created by Bernard Désert on 10/18/17.
+//  Copyright © 2017 Bernard Désert. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#import "AFNetworking/AFNetworking.h"
+
+#endif /* bridge_h */
