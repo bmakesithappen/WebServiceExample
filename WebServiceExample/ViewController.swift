6fe2e5bbd6c033e7920a788b3a66fa63c1542e98
Bernard Desert <bdesert@etsy.com>
2017-11-06 13:18:33 -0500

    Redid manager.get updated API key and api request

--- WebServiceExample/ViewController.swift
+++ WebServiceExample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
    // API Key 883f34fa10bc4cf6dcf044fae47008ad
   // TODO: Add SwiftyJSON https://github.com/SwiftyJSON/SwiftyJSON
+  
     
     @IBOutlet weak var forecastLabel: UILabel!
     
@@ -21,19 +22,20 @@ class ViewController: UIViewController {
         
         let manager = AFHTTPSessionManager()
         
-        manager.get("http://api.openweathermap.org/data/2.5/weather?q=London&mode=json&units=metric&cnt=1&appid=883f34fa10bc4cf6dcf044fae47008ad",
+        
+        manager.get("http://api.openweathermap.org/data/2.5/weather?q=London&mode=json&units=metric&cnt=1&appid=883f34fa10bc4cf6dcf044fae47008ad", // this is a different ap request than the one listed in the lesson please @AHALL
                     parameters: nil,
                     progress: nil,
                     success: { (operation: URLSessionDataTask, responseObject:Any?) in
                         if let responseObject = responseObject {
                             print("Response: " + (responseObject as AnyObject).description)
-                            if let listOfDays = (responseObject as AnyObject)["list"] as? [AnyObject] {
-                                if let tomorrow = listOfDays[0] as? [String:AnyObject] {
-                                    if let tomorrowsWeather = tomorrow["weather"] as? [AnyObject] {
-                                        if let firstWeatherOfDay = tomorrowsWeather[0] as? [String:AnyObject] {
-                                            if let forecast = firstWeatherOfDay["description"] as? String {
-                                                self.forecastLabel.text = forecast
-                                            }
+                        }
+                        if let listOfDays = (responseObject as AnyObject)["list"] as? [AnyObject] {
+                            if let tomorrow = listOfDays[0] as? [String:AnyObject] {
+                                if let tomorrowsWeather = tomorrow["weather"] as? [AnyObject] {
+                                    if let firstWeatherOfDay = tomorrowsWeather[0] as? [String:AnyObject] {
+                                        if let forecast = firstWeatherOfDay["description"] as? String {
+                                            self.forecastLabel.text = forecast
                                         }
                                     }
                                 }
@@ -41,10 +43,10 @@ class ViewController: UIViewController {
                         }
         }) { (operation:URLSessionDataTask?, error:Error) in
             print("Error: " + error.localizedDescription)
-            
         }
+        
     }
-    
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
